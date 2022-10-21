57b9689db4731d2bdc799d0474c8708f8397fa7a
makuachteny <m.gatluak@alustudent.com>
2022-10-21 03:39:20 -0700

    task 7-edges.py added to Python_programming

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = word[:3]
-word_first_2 = word[-2:]
-middle_word = word[1:-1]
-print(f"First 3 letters: {word_first_3}")
-print(f"Last 2 letters: {word_last_2}")
-print(f"Middle word: {middle_word}")
+first3 = word[:3]
+last2 = word[-2:]
+mid = word[1:-1]
+print("First 3 letters: {}".format(first3))
+print("Last 2 letters: {}".format(last2))
+print("Middle word: {}".format(mid))
