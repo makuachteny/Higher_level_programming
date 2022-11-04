42f42193a0f142cd0e2ddb7affcba9d3d0275981
makuachteny <m.gatluak@alustudent.com>
2022-11-04 05:30:02 -0700

    Mandatory task added

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -9,7 +9,7 @@ def best_score(a_dictionary):
     ret = list(a_dictionary.keys())[0]
     big = a_dictionary[ret]
     for k, v in a_dictionary.items():
-        if v > big       
+        if v > big:       
             big = v
             ret = k
-        return (ret)
+    return (ret)
