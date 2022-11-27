50fbf2910d29e2956cbadc03e4bd778da976c0c0
makuachteny <m.gatluak@alustudent.com>
2022-11-26 21:54:35 -0800

    Mandatory task added

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,8 +3,5 @@
 
 def search_replace(my_list, search, replace):
     """Replace all occurrences of an element by another in a new list."""
-    new_list = my_list[:]
-    for i in range(len(new_list)):
-        if new_list[i] == search:
-            new_list[i] = replace
-        return (new_list)
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return (new_list)
