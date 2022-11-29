55bf52763f687a1d825285e177f88af7aa0905cf
makuachteny <m.gatluak@alustudent.com>
2022-11-29 10:19:47 -0800

    Mandatory task added

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Script that creates a database and a table on MysQL
+-- Creates the database hbtn_0d_usa and table states created on MySQL server
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+	)
