e6ef33bc377e45ba62ccf0c7efd9301142a7f9bc
makuachteny <m.gatluak@alustudent.com>
2022-12-08 02:25:31 -0800

    Mandatory task added

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
-"""This module defines the read_lines function"""
+"""Append to a file."""
 
 
-def read_lines(filename="", nb_lines=0):
-    """Reads n lines of a text file (UTF8) and prints it to stdout
-    Args:
-    filename (str): Filename
-    nb_lines (int): number of lines to read
-    """
-    with open(filename, encoding='utf-8') as file:
-        if nb_lines <= 0:
-            for line in file:
-                print(line, end="")
-            else:
-                for lines in range(nb_lines):
-                    print(file.readline(), end="")
+def append_write(filename="", text=""):
+    """Append to a file."""
+    with open(filename, 'a+') as f:
+        return f.write(text)
