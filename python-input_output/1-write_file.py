5713685de10c8046ec9f92e72457612de9c70b59
makuachteny <m.gatluak@alustudent.com>
2022-12-08 02:20:39 -0800

    Mandatory task added

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-"""This module defines the number_of_lines function"""
+"""write to a file."""
 
 
-def number_of_lines(filename=""):
-    """Returns the number of lines of a text file
-    Args:
-    filename (str): Filename
-    """
-    count = 0
-    with open(filename, encoding='utf-8') as file:
-        for line in file:
-            count += 1
-        return count
+def write_file(filename="", text=""):
+    """Write to a file."""
+    with open(filename, 'w+') as f:
+        return f.write(text)
