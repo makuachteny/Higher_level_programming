e628ce04d19b0d4618bdee1219946401ef7ff16e
makuachteny <m.gatluak@alustudent.com>
2022-12-07 19:47:57 -0800

    Mandatory task added

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -12,16 +12,20 @@ def matrix_divided(matrix, div):
         List: New matrix
     """
     if type(matrix) != list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/fl\oats")
+        raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/fl\oats")
     for row in matrix:
         if type(row) != list:
-            raise TypeError("matrix must be a matrix (list of lists) of \integers/floats")
+            raise TypeError(
+                    "matrix must be a matrix (list of lists) of \integers/floats")
     if len(matrix) == 0:
-        raise TypeError("matrix must be a matrix (list of lists) of \integers/floats")
+        raise TypeError(
+                "matrix must be a matrix (list of lists) of \integers/floats")
     for row in matrix:
         for i in row:
             if (type(i) != int and type(i) != float) or i != i:
-                raise TypeError("matrix must be a matrix (list of lists) of\integers/floats")
+                raise TypeError(
+                        "matrix must be a matrix (list of lists) of\integers/floats")
             if type(div) != int and type(div) != float:
                 raise TypeError("div must be a number")
             if div != div or div == float('inf'):
@@ -33,9 +37,11 @@ def matrix_divided(matrix, div):
                 if len(row) != 0:
                     nonemptyflag = 1
                 if nonemptyflag == 0:
-                    raise TypeError("matrix must be a matrix (list of lists) of \integers/floats")
+                    raise TypeError(
+                            "matrix must be a matrix (list of lists) of \integers/floats")
                 length = len(matrix[0])
             for row in matrix:
                     if len(row) != length:
-                        raise TypeError("Each row of the matrix must have the same size")
+                        raise TypeError(
+                                "Each row of the matrix must have the same size")
             return [[round(i/div, 2) for i in row] for row in (matrix)]
