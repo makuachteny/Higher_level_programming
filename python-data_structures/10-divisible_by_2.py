3974b4fbcdbfacd38d2c85265cb7c67a2286a84f
makuachteny <m.gatluak@alustudent.com>
2022-12-09 21:51:29 -0800

    Mandatory task added

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -6,5 +6,5 @@ def divisible_by_2(my_list=[]):
         if my_list[i] % 2 == 0:
             multiples.append(True)
         else:
-             multiples.append(False) 
+            multiples.append(False)
     return (multiples)
