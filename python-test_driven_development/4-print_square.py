8a87011363925593d4e0b6af877defb0611079cb
Makuach Teny Gatluak <m.gatluak@alustudent.com>
2023-01-26 13:10:27 +0300

    Update 4-print_square.py

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -4,16 +4,11 @@
 
 
 def print_square(size):
-    """ Function that prints a square.
-    Args:
-        size (int): size of square
-    """
-if type(size) != int or size != size:
-    raise TypeError("size must be an integer")
-if size < 0:
-    raise ValueError("size must be >= 0")
-if size > 0:
-    for row in range(size):
-        for column in range(size):
-            print("#", end="")
-        print()
+    """Prints a square with the # character"""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
+        print("")
