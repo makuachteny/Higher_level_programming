67950451d5198801280c19c8e3e6ae2def8c8220
makuachteny <m.gatluak@alustudent.com>
2023-02-01 06:35:14 -0800

    Mandatory task added

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -3,5 +3,5 @@
 // The process.arg array contains the command line arguments passed to the Node.js scripy
 // The '?' is a ternary operator that checks the undefined value of process.argv
 
-console.log(process.arg.slice[2] === undefined ? 'No argument' : process.arg[2])
+console.log(process.arg.slice[2] === undefined ? 'No argument' : process.arg[2]);
 
