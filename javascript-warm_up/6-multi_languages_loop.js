0f2dbf1ac20e6a0ea9a1999b94d578d10a2cbaf9
makuachteny <m.gatluak@alustudent.com>
2023-02-02 10:27:52 +0300

    🐛🐛

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const string = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+var.forEach((value, index) => {
+  console.log(string[index])
+});
---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (isNaN(process.argv[2]) || process.argv[2] === undefined)
+  { console.log('Missing number of occurences');}
+  else {
+    const x = Number(process.arg[2]);
+     let i = 0;
+  while (i < x) {
+    console.log('C is fun');
+    i++;
+  }
