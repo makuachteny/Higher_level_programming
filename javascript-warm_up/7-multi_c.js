9eabcdf3c47b7ea598ac3a5742ed73a868e7bdd9
makuachteny <m.gatluak@alustudent.com>
2023-02-06 11:49:36 +0300

    X defined again

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,7 +2,10 @@
 let x;
 if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
   console.log('Missing number of occurrences');
-} else x = Number(process.argv[2]);
+  x = 0;
+} else {
+  x = Number(process.argv[2]);
+}
 let i = 0;
 while (i < x) {
   console.log('C is fun');
