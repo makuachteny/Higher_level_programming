4d04c03166e7a0a30b9f17cda222fc374e0e2be8
makuachteny <m.gatluak@alustudent.com>
2023-02-06 10:59:25 +0300

    Debugged loop

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
-if (isNaN(process.argv[2]) || process.argv[2] === undefined)
-  { console.log('Missing number of occurences');}
-  else {
-    const x = Number(process.arg[2]);
-     let i = 0;
+
+const x = Number(process.arg[2]);
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  let i = 0;
   while (i < x) {
     console.log('C is fun');
     i++;
-  };
+  }
+}
