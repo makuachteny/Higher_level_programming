0a0afe431a19aad92844cee8ae60cb6f4f4e6133
makuachteny <m.gatluak@alustudent.com>
2023-02-08 15:03:33 +0300

    Logical OR (||) added before typeof

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,14 +1,19 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor(width, height) {
-        if (width = 0 || height = 0 typeof width !== 'number' typeof height !== 'number') {
-            this.width = 0;
-            this.height = 0;
-        } else {
-            this.width = w;
-            this.height = h;
-        }
+  constructor (width, height) {
+    if (
+      width === 0 ||
+      height === 0 ||
+      typeof width !== 'number' ||
+      typeof height !== 'number'
+    ) {
+      this.width = 0;
+      this.height = 0;
+    } else {
+      this.width = w;
+      this.height = h;
     }
+  }
 }
 module.exports = Rectangle;
