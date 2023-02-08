5ad5eb4c593e7029da3cf9ef2c07222f1af1221d
makuachteny <m.gatluak@alustudent.com>
2023-02-08 14:39:22 +0300

    Mandatory task added

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+class Rectangle {
+    constructor(width, height) {
+        if (width = 0 || height = 0 typeof width !== 'number' typeof height !== 'number') {
+            this.width = 0;
+            this.height = 0;
+        } else {
+            this.width = w;
+            this.height = h;
+        }
+    }
+}
