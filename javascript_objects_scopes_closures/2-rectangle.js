601f8d92170ec0d21c9eb18d4165e0ee410f7526
makuachteny <m.gatluak@alustudent.com>
2023-02-08 15:24:32 +0300

    Debugging

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,13 @@
-#!/usr/bin/node
-
 class Rectangle {
-  constructor (w, h) {
-    if (w === 0 || h === 0 || typeof w !== 'number' || typeof h !== 'number') {
+  constructor(w, h) {
+    if (
+      w <= 0 ||
+      h <= 0 ||
+      typeof w !== 'number' ||
+      typeof h !== 'number' ||
+      !Number.isInteger(w) ||
+      !Number.isInteger(h)
+    ) {
       this.width = 0;
       this.height = 0;
     } else {
