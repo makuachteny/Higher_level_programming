d158c83eb6943c696f126cb86f75ae5a1c5ef1e7
makuachteny <m.gatluak@alustudent.com>
2023-02-09 10:02:15 +0300

    Mandatory task added

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const Square = require('./5-square.js');
+
+class Square extends Square {
+  constructor(size) {
+    super(size, size);
+  }
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
