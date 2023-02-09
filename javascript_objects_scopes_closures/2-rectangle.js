fd42019433da033df412ef3f13cec85fb327430f
makuachteny <m.gatluak@alustudent.com>
2023-02-09 07:56:27 +0300

    'w' and 'h' defined

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,21 +1,11 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
-    if (
-      w == 0 ||
-      h == 0 ||
-      typeof w != 'number' ||
-      typeof h != 'number' ||
-      !Number.isInteger(w) ||
-      !Number.isInteger(h)
-    ) {
-      this.width = 0;
-      this.height = 0;
-    } else {
-      this.width = w;
-      this.height = h;
-    }
+  constructor (w, h) {
+    if (w == null || h == null) return null;
+    if (w <= 0 || h <= 0) return null;
+    this.width = w;
+    this.height = h;
   }
 }
 module.exports = Rectangle;
