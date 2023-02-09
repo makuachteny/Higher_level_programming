719ac42975ca9c528646093801bb82b59036d48d
makuachteny <m.gatluak@alustudent.com>
2023-02-09 08:29:27 +0300

    Mandatory task added

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -2,16 +2,10 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (
-      w === 0 ||
-      h === 0 ||
-      typeof w !== 'number' ||
-      typeof h !== 'number' ||
-      !Number.isInteger(w) ||
-      !Number.isInteger(h)
-    ) {
-      this.width = 0;
-      this.height = 0;
+    if (w == null || h == null) {
+      return null;
+    } else if (w <= 0 || h <= 0) {
+      return null;
     } else {
       this.width = w;
       this.height = h;
@@ -19,8 +13,10 @@ class Rectangle {
   }
 
   print () {
-    console.log('X'.repeat(this.width));
-    console.log('\n'.repeat(this.height - 1));
+    for (let i = 0; i < this.height; i++) {
+      console.log('x'.repeat(this.width));
+    }
   }
 }
+
 module.exports = Rectangle;
